ecfb9bf0480603c2e65395fa3b8694a90b0adbd7
Kiran Kumar Thommandru <kirankumarthommandru2@gmail.com>
2022-06-17 17:59:51 +0530

    some more changes

--- Neovim.cpp
+++ Neovim.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main(int argc, char *argv[])
-{	
+{
+	#ifndef Output
+	 freopen("in.txt", "r", stdin);
+	 freopen("out.txt", "w", stdout);
+	#endif
+	
 	cout << "Message" << endl;
 	return 0;
 }
